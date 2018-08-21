ceee25f45bded730960a92999f6176c884a982b8
freewind <nowindlee@gmail.com>
2018-08-21 21:56:23 +0800

    demo

---
+++ src/main/java/demo/Hello.java
@@ -0,0 +1,26 @@
+package demo;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import java.io.IOException;
+
+public class Hello {
+
+    private static OkHttpClient client = new OkHttpClient();
+
+    public static void main(String[] args) throws IOException {
+        Request request = new Request.Builder()
+                .url("https://api.github.com/repos/freewind/java-github-get-topics-demo/topics")
+                .header("Accept", "application/vnd.github.mercy-preview+json")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String body = response.body().string();
+        System.out.println(body);
+    }
+
+}
